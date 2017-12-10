9796d9f1f8f3352ade594c8a5c20f32b0140cf34
Sergio Cinos <sergio.cinos@gmail.com>
2017-12-11 07:03:11 +1100

    Day 06, challenge 01 and 02

---
+++ src/06/solution.js
@@ -0,0 +1,51 @@
+const findBiggestBank = memory => memory.reduce(
+  (biggestIndex, blocks, index) => {
+    if (blocks > memory[biggestIndex]) return index;
+    return biggestIndex;
+  },
+  0,
+);
+
+const balanceMemory = (memory) => {
+  const workingMemory = Array.of(...memory);
+
+  let memoryBank = findBiggestBank(workingMemory);
+  let blocks = workingMemory[memoryBank];
+  // Reset current bank
+  workingMemory[memoryBank] = 0;
+
+  while (blocks > 0) {
+    // Get next memory position
+    memoryBank = (memoryBank + 1) % workingMemory.length;
+
+    // Move one block to the bank
+    workingMemory[memoryBank] += 1;
+    blocks -= 1;
+  }
+
+  return workingMemory;
+};
+
+const findLoop = (memory) => {
+  const knownConfigurations = {};
+  let counter = 0;
+  while (true) {
+    // Generate memory configuration id
+    const id = memory.join(':');
+
+    // Check if the memory is a known configuration
+    if (id in knownConfigurations) return { counter, size: counter - knownConfigurations[id] };
+    knownConfigurations[id] = counter;
+
+    // Process memory and do the whole thing again
+    memory = balanceMemory(memory);
+    counter += 1;
+  }
+};
+
+const challenge1 = input => findLoop(input.split(/\s+/).map(Number)).counter;
+const challenge2 = input => findLoop(input.split(/\s+/).map(Number)).size;
+
+module.exports = {
+  challenge1, challenge2, balanceMemory, findBiggestBank,
+};
---
+++ src/06/test.js
@@ -0,0 +1,80 @@
+const {
+  challenge1, challenge2, balanceMemory, findBiggestBank,
+} = require('./solution.js');
+const { expect } = require('chai');
+const path = require('path');
+
+const readInput = require('../lib/readInput')(path.join(__dirname, 'input.txt'));
+
+describe('Day 06 - Challenge 01', () => {
+  it('Example #01: 0, 2, 7, 0', () => {
+    const memory = [0, 2, 7, 0];
+    const biggestBank = findBiggestBank(memory);
+    expect(biggestBank).to.equal(2);
+
+    const balancedMemory = balanceMemory(memory);
+    expect(balancedMemory).to.deep.equal([2, 4, 1, 2]);
+  });
+
+  it('Example #02: 2, 4, 1, 2', () => {
+    const memory = [2, 4, 1, 2];
+    const biggestBank = findBiggestBank(memory);
+    expect(biggestBank).to.equal(1);
+
+    const balancedMemory = balanceMemory(memory);
+    expect(balancedMemory).to.deep.equal([3, 1, 2, 3]);
+  });
+
+  it('Example #03: 3, 1, 2, 3', () => {
+    const memory = [3, 1, 2, 3];
+    const biggestBank = findBiggestBank(memory);
+    expect(biggestBank).to.equal(0);
+
+    const balancedMemory = balanceMemory(memory);
+    expect(balancedMemory).to.deep.equal([0, 2, 3, 4]);
+  });
+
+  it('Example #04: 0, 2, 3, 4', () => {
+    const memory = [0, 2, 3, 4];
+    const biggestBank = findBiggestBank(memory);
+    expect(biggestBank).to.equal(3);
+
+    const balancedMemory = balanceMemory(memory);
+    expect(balancedMemory).to.deep.equal([1, 3, 4, 1]);
+  });
+
+  it('Example #05: 1, 3, 4, 1', () => {
+    const memory = [1, 3, 4, 1];
+    const biggestBank = findBiggestBank(memory);
+    expect(biggestBank).to.equal(2);
+
+    const balancedMemory = balanceMemory(memory);
+    expect(balancedMemory).to.deep.equal([2, 4, 1, 2]);
+  });
+
+  it('Example #06: 0, 2, 7, 0', () => {
+    const memory = '0 2 7 0';
+    const loopIndex = challenge1(memory);
+    expect(loopIndex).to.equal(5);
+  });
+
+  it('Puzzle input', async () => {
+    const input = await readInput();
+    const result = challenge1(input);
+    expect(result).to.equal(7864);
+  });
+});
+
+describe('Day 06 - Challenge 02', () => {
+  it('Example #01: 0, 2, 7, 0', () => {
+    const memory = '0 2 7 0';
+    const loopSize = challenge2(memory);
+    expect(loopSize).to.equal(4);
+  });
+
+  it('Puzzle input', async () => {
+    const input = await readInput();
+    const result = challenge2(input);
+    expect(result).to.equal(1695);
+  });
+});
