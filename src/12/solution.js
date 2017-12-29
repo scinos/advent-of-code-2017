87a0f2a83b581f7bb13ec799f57971225d0ff0f5
Sergio Cinos <sergio.cinos@gmail.com>
2017-12-29 18:31:40 +1100

    Challenge 12, day 1 and 2

---
+++ src/12/solution.js
@@ -0,0 +1,60 @@
+const regroupTree = (tree) => {
+  let changed;
+  do {
+    changed = false;
+    Object.values(tree).forEach((node) => {
+      const newGroupId = Math.min(node.id, ...node.peers.map(p => tree[p].groupId));
+      if (newGroupId !== node.groupId) {
+        node.groupId = newGroupId;
+        changed = true;
+      }
+    });
+  } while (changed);
+};
+
+const parseInput = (input) => {
+  const re = /^([0-9]+) <-> (.*)$/;
+  const nodes = input
+    .split('\n')
+    .map((line) => {
+      const [, id, pipes] = line.match(re);
+      const peers = pipes.replace(/ /g, '').split(',').map(Number);
+      return {
+        id: Number(id),
+        peers,
+        groupId: Number(id),
+      };
+    });
+
+  regroupTree(nodes);
+  return nodes;
+};
+
+const isInGroup = (input, group, number) => {
+  const tree = (typeof input === 'string')
+    ? parseInput(input)
+    : input;
+
+  return Object
+    .values(tree)
+    .filter(node => node.groupId === group)
+    .map(node => node.id)
+    .includes(number);
+};
+
+const challenge1 = (input) => {
+  const nodes = parseInput(input);
+  return Object.values(nodes)
+    .filter(node => node.groupId === 0).length;
+};
+
+const challenge2 = (input) => {
+  const nodes = parseInput(input);
+  return (new Set(Object.values(nodes).map(node => node.groupId))).size;
+};
+
+module.exports = {
+  isInGroup,
+  challenge1,
+  challenge2,
+};
---
+++ src/12/test.js
@@ -0,0 +1,62 @@
+const {
+  challenge1,
+  isInGroup,
+  challenge2,
+} = require('./solution.js');
+const { expect } = require('chai');
+const path = require('path');
+
+const readInput = require('../lib/readInput')(path.join(__dirname, 'input.txt'));
+
+describe('Day 12 - Challenge 01', () => {
+  const exampleInput = [
+    '0 <-> 2',
+    '1 <-> 1',
+    '2 <-> 0, 3, 4',
+    '3 <-> 2, 4',
+    '4 <-> 2, 3, 6',
+    '5 <-> 6',
+    '6 <-> 4, 5',
+  ].join('\n');
+
+  it('Example 1', () => expect(isInGroup(exampleInput, 0, 0)).to.equal(true));
+  it('Example 2', () => expect(isInGroup(exampleInput, 0, 1)).to.equal(false));
+  it('Example 3', () => expect(isInGroup(exampleInput, 0, 2)).to.equal(true));
+  it('Example 4', () => expect(isInGroup(exampleInput, 0, 3)).to.equal(true));
+  it('Example 5', () => expect(isInGroup(exampleInput, 0, 4)).to.equal(true));
+  it('Example 6', () => expect(isInGroup(exampleInput, 0, 5)).to.equal(true));
+  it('Example 7', () => expect(isInGroup(exampleInput, 0, 6)).to.equal(true));
+
+  it('Full example', () => {
+    expect(challenge1(exampleInput)).to.equal(6);
+  });
+
+  it('Puzzle input', async () => {
+    const input = await readInput();
+    const result = challenge1(input);
+    expect(result).to.equal(169);
+  });
+});
+
+describe('Day 12 - Challenge 02', () => {
+  const exampleInput = [
+    '0 <-> 2',
+    '1 <-> 1',
+    '2 <-> 0, 3, 4',
+    '3 <-> 2, 4',
+    '4 <-> 2, 3, 6',
+    '5 <-> 6',
+    '6 <-> 4, 5',
+  ].join('\n');
+
+  it('Full example', () => {
+    expect(challenge2(exampleInput)).to.equal(2);
+  });
+
+  it('Puzzle input', async () => {
+    const input = await readInput();
+    const result = challenge2(input);
+    expect(result).to.equal(179);
+  });
+});
+
