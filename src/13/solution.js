8d34a5c4949f5dbffe97e09bb2434e4ca16ae772
Sergio Cinos <sergio.cinos@gmail.com>
2017-12-30 17:49:24 +1100

    Day 13, challenge 1 and 2

---
+++ src/13/solution.js
@@ -0,0 +1,92 @@
+const moveScannerUp = (layer) => {
+  layer.direction = 'up';
+  layer.position = Math.max(0, layer.position - 1);
+};
+
+const moveScannerDown = (layer) => {
+  layer.direction = 'down';
+  layer.position = Math.min(layer.range - 1, layer.position + 1);
+};
+
+const moveScanner = (layer) => {
+  if (layer.direction === 'down') {
+    if (layer.position === layer.range - 1) {
+      moveScannerUp(layer);
+    } else {
+      moveScannerDown(layer);
+    }
+  } else if (layer.direction === 'up') {
+    if (layer.position === 0) {
+      moveScannerDown(layer);
+    } else {
+      moveScannerUp(layer);
+    }
+  }
+};
+
+const movePacket = (packet, scanner) => {
+  packet.layer += 1;
+  const layer = scanner[packet.layer];
+
+  if (typeof layer === 'object' && layer.position === 0) {
+    packet.cost += (packet.layer * layer.range);
+  }
+};
+
+const parseInput = (input) => {
+  const re = /^(\d*?): (\d*?)$/;
+
+  const recording = input.split('\n')
+    .map(line => line.match(re))
+    .map(([, depth, range]) => ({ depth: Number(depth), range: Number(range) }));
+
+  const firewall = [];
+  recording.forEach(({ depth, range }) => {
+    firewall[depth] = {
+      range,
+      position: 0,
+      direction: 'down',
+    };
+  });
+  return firewall;
+};
+
+
+const crossFirewall = (firewall) => {
+  const packet = {
+    layer: -1,
+    cost: 0,
+  };
+  while (packet.layer < firewall.length) {
+    movePacket(packet, firewall);
+    for (let index = 0; index < firewall.length; index += 1) {
+      const f = firewall[index];
+      if (!f) continue;
+      moveScanner(f);
+    }
+  }
+
+  return packet.cost;
+};
+
+module.exports.challenge1 = (input) => {
+  const firewall = parseInput(input);
+  return crossFirewall(firewall);
+};
+
+const layerIsZero = (range, delay, index) => {
+  const period = (range - 1) * 2;
+  return (delay + index) % period === 0;
+};
+
+module.exports.challenge2 = (input) => {
+  const firewall = parseInput(input);
+
+  const hasZeroForDelay = delay =>
+    firewall.some(({ range }, index) => layerIsZero(range, delay, index));
+
+  let delay = 0;
+  while (hasZeroForDelay(delay)) { delay += 1; }
+  return delay;
+};
+
---
+++ src/13/test.js
@@ -0,0 +1,47 @@
+const {
+  challenge1,
+  challenge2,
+} = require('./solution.js');
+const { expect } = require('chai');
+const path = require('path');
+
+const readInput = require('../lib/readInput')(path.join(__dirname, 'input.txt'));
+
+describe('Day 13 - Challenge 01', () => {
+  const exampleInput = [
+    '0: 3',
+    '1: 2',
+    '4: 4',
+    '6: 4',
+  ].join('\n');
+
+  it('Full example', () => {
+    expect(challenge1(exampleInput)).to.equal(24);
+  });
+
+  it('Puzzle input', async () => {
+    const input = await readInput();
+    const result = challenge1(input);
+    expect(result).to.equal(1624);
+  });
+});
+
+describe('Day 13 - Challenge 02', () => {
+  const exampleInput = [
+    '0: 3',
+    '1: 2',
+    '4: 4',
+    '6: 4',
+  ].join('\n');
+
+  it('Full example', () => {
+    expect(challenge2(exampleInput)).to.equal(10);
+  });
+
+  it('Puzzle input', async () => {
+    const input = await readInput();
+    const result = challenge2(input);
+    expect(result).to.equal(3923436);
+  });
+});
+
